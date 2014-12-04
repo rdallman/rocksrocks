39d99263b05a4bb88cdbc0067b3984cc535f8051
Reed Allman <rdallman10@gmail.com>
2014-12-03 23:39:54 -0800

    store interface for queue stuffs

--- queue_test.go
+++ queue_test.go
@@ -2,30 +2,49 @@ package queue_test
 
 import (
 	"bytes"
+	"os/exec"
 	"testing"
 
 	"github.com/rdallman/rocksrocks/queue"
 	"github.com/rdallman/rocksrocks/rocks"
+	"gopkg.in/inconshreveable/log15.v2"
 )
 
 // rawr import cycles
 
-func benchmarkEnqueueSize(s queue.Store, n int, b *testing.B) {
-	defer s.Destroy()
+func bod(size int) []byte {
 	var buf bytes.Buffer
-	for i := 0; i < n; i++ {
+	for i := 0; i < size; i++ {
 		buf.WriteString("a")
 	}
-	body := buf.Bytes()
+	return buf.Bytes()
+}
+
+func benchmarkEnqueueSize(s queue.Store, n int, b *testing.B) {
+	defer s.Destroy()
+	body := bod(n)
 	b.SetBytes(int64(len(body)))
 	q := queue.New("hi")
 
 	b.ResetTimer()
+	b.SetParallelism(50)
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			q.Enqueue(s, body)
 		}
 	})
+}
+
+func benchmarkEnqueueN(s queue.Store, n int, b *testing.B) {
+	defer s.Destroy()
+
+	body := bod(1024 * 1024)
+	q := queue.New("hi")
+	for i := 0; i < n; i++ {
+		q.Enqueue(s, body)
+	}
+	out, _ := exec.Command("du", "-h", "tmpdata").CombinedOutput()
+	log15.Info(string(out))
 }
 
 //func BenchmarkEnqueueLevel1(b *testing.B) {
@@ -35,3 +54,11 @@ func benchmarkEnqueueSize(s queue.Store, n int, b *testing.B) {
 func BenchmarkEnqueueRocks1(b *testing.B) {
 	benchmarkEnqueueSize(rocks.NewStore(), 1024*1024, b)
 }
+
+//func BenchmarkEnqueueLevel1000(b *testing.B) {
+//benchmarkEnqueueN(level.NewStore(), 10000, b)
+//}
+
+//func BenchmarkEnqueueRocks1000(b *testing.B) {
+//benchmarkEnqueueN(rocks.NewStore(), 10000, b)
+//}
--- rocks/rocks.go
+++ rocks/rocks.go
@@ -21,7 +21,8 @@ type Queue struct {
 
 type rocksdb struct {
 	*gorocksdb.DB
-	dir string
+	opts *gorocksdb.Options
+	dir  string
 }
 
 func NewStore() *rocksdb {
@@ -29,17 +30,43 @@ func NewStore() *rocksdb {
 	opts := gorocksdb.NewDefaultOptions()
 	opts.SetCreateIfMissing(true)
 	opts.IncreaseParallelism(runtime.NumCPU())
-	opts.OptimizeLevelStyleCompaction(512 * 1024 * 1024)
+	//opts.OptimizeUniversalStyleCompaction(512 * 1024 * 1024)
+
+	//bpl=10485760;overlap=10;mcz=2;del=300000000;levels=6;ctrig=4; delay=8; stop=12; wbn=3; mbc=20;
+	//mb=67108864;wbs=134217728; dds=0; sync=0; r=1000000000; t=1; vs=800; bs=65536; cs=1048576; of=500000; si=1000000;
+
+	//--block_size=$bs --cache_size=$cs --bloom_bits=10 --cache_numshardbits=4 --open_files=$of
+	//--verify_checksum=1 --sync=$sync --disable_wal=1 --compression_type=zlib --stats_interval=$si
+	//--compression_ratio=50 --disable_data_sync=$dds --write_buffer_size=$wbs --target_file_size_base=$mb --max_write_buffer_number=$wbn
+	//--max_background_compactions=$mbc --level0_file_num_compaction_trigger=$ctrig
+	//--level0_slowdown_writes_trigger=$delay --level0_stop_writes_trigger=$stop
+	//--num_levels=$levels --delete_obsolete_files_period_micros=$del --min_level_to_compress=$mcz
+	//--max_grandparent_overlap_factor=$overlap --stats_per_interval=1 --max_bytes_for_level_base=$bpl --use_existing_db=0
+
+	//opts.OptimizeLevelStyleCompaction(512 * 1024 * 1024)
+	//opts.SetLevel0SlowdownWritesTrigger(-1) // don't
+	//opts.SetLevel0StopWritesTrigger(20)     // don't
+
+	opts.SetWriteBufferSize(64 * 1024 * 1024)
+	opts.SetMaxWriteBufferNumber(3)
+	opts.SetTargetFileSizeBase(64 * 1024 * 1024)
+	opts.SetLevel0FileNumCompactionTrigger(8)
+	opts.SetLevel0SlowdownWritesTrigger(17)
+	opts.SetLevel0StopWritesTrigger(24)
+	opts.SetNumLevels(4)
+	opts.SetMaxBytesForLevelBase(512 * 1024 * 1024)
+	opts.SetMaxBytesForLevelMultiplier(8)
+	opts.SetCompression(4)
 
 	bbto := gorocksdb.NewDefaultBlockBasedTableOptions()
 	bbto.SetBlockCache(gorocksdb.NewLRUCache(2048 * 1024 * 1024))
-	bbto.SetFilterPolicy(gorocksdb.NewBloomFilter(20))
-	opts.SetBlockBasedTableFactory(bbto)
+	bbto.SetFilterPolicy(gorocksdb.NewBloomFilter(10))
 
+	opts.SetBlockBasedTableFactory(bbto)
 	opts.SetMaxOpenFiles(-1)
-	opts.SetWriteBufferSize(128 * 1024 * 1024)
+
 	//opts.SetPrefixExtractor(gorocksdb.NewFixedPrefixTransform(8))
-	//opts.SetMemtablePrefixBloomBits(100000000)
+	opts.SetMemtablePrefixBloomBits(8 * 1024 * 1024)
 	//opts.SetMergeOperator(new(counterMergeOperator))
 
 	if err := os.MkdirAll("./tmpdata", 0755); err != nil {
@@ -50,7 +77,7 @@ func NewStore() *rocksdb {
 		log15.Crit("bad things", "err", err)
 		os.Exit(1)
 	}
-	return &rocksdb{DB: db, dir: "./tmpdata"}
+	return &rocksdb{DB: db, dir: "./tmpdata", opts: opts}
 }
 
 var (
